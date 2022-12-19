abf99e277e0059e5ea55c344302ef0b859b19d0d
Lazola2 <lazolamakubalo271@gmail.com>
2022-12-19 14:14:45 +0200

    Initial commit

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="profile-card">
+        <div class="profile-image">
+            <img :src="userData.picture" :alt="userData.name">
+        </div>
+        <div class="profile-info">
+            <span>Written By</span>
+            <h3>{{userData.name}}</h3>
+            <p v-text="userData.bio"></p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {ref} from 'vue'
+    const loadUserData = async () => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({
+                   name: 'Lazola Makubalo',
+                   picture: '<link>',
+                   bio: 'A web and a software developer'
+                })
+            }, 1000)
+        })
+    }
+
+    export default {
+        name: 'ProfileCard',
+        async setup() {
+            const userData = ref(await loadUserData());
+            return {userData}
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Suspense>
+    <template #default>
+      <ProfileCard/>
+    </template>
+    <template #fallback>
+      <h1>Loading...</h1>
+    </template>
+  </Suspense>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import ProfileCard from '@/components/ProfileCard.vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ProfileCard
   }
 }
 </script>
